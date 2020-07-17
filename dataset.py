52be0e14c9d4f99a69afcf379342c4a7dcef8255
AoiHosizora <a970335605@hotmail.com>
2020-07-17 23:49:13 +0800

    feat(*): further improve dataset.py and unet.py

--- dataset.py
+++ dataset.py
@@ -10,6 +10,8 @@ class SSDataset(Dataset):
     AVM system SS (Semantic Segmentation) dataset
     """
 
+    all_colors = []
+
     def __init__(self, dataset_path, is_train):
         self.is_train = is_train
         self.dataset_path = dataset_path
@@ -21,12 +23,17 @@ class SSDataset(Dataset):
 
         self.dataset = []
         for db_line in db_file.split('\n'):
-            db_image, db_gt = db_line.split(' ')[:2]
+            sp = db_line.split(' ')
+            if len(sp) < 2:
+                continue
+            db_image, db_gt = sp[:2]
             db_image, db_gt = self._path(db_image), self._path(db_gt)
-            # print(db_image, db_gt)
+            print(db_image, db_gt)
             db_image = self._read_image(db_image)
             db_gt = self._read_gt(db_gt)
             self.dataset.append((db_image, db_gt))
+
+        self._parse_gt()
 
         print(('Train' if is_train else 'Test') + 'dataset: image count = {}'.format(self.__len__()))
 
@@ -42,29 +49,65 @@ class SSDataset(Dataset):
         return os.path.join(self.dataset_path, name)
 
     def _read_image(self, image_path):
+        """
+        Read dataset/images/00000000.jpg, transpose to c,w,h
+        """
         image = Image.open(image_path)
-        image = np.array(image)  # w h c
-        image = image.transpose((2, 0, 1))  # c w h
+        image = np.array(image)  # h w c
+        image = image.transpose((2, 1, 0))  # c w h
         return image / 255  # normalization
 
-    def _read_gt(self, gt):
+    def _read_gt(self, image_path):
+        """
+        Read dataset/gt/00000000.jpg, transpose to w,h,c
+        """
         image = Image.open(image_path)
-        image = np.array(image)  # w h c
-        image_w, image_h = image.shape()[1:3]
+        image = np.array(image)  # h w c
+        image = image.transpose((1, 0, 2))  # w h c
+        return image
 
-        classes = {}
-        for w in range(image_w):
-            for h in range(image_h):
-                r, g, b = image[w][h][:3]
-                if (r, g, b) not in classes:
-                    classes[(r, g, b)] = []
-                classes[(r, g, b)].append((w, h))
+    def _parse_gt(self):
+        """
+        Parse a list of gt images, get all_colors list, and split class to different feature channels
+        """
+        # get final all_colors list
+        if len(self.all_colors) == 0:
+            target_colors = []
+            target_idx = 0
+            for idx, (_, gt_image) in enumerate(self.dataset):
+                image_w, image_h = gt_image.shape[0:2]
+                pixels = [tuple(a) for b in gt_image.tolist() for a in b]
+                current_colors = list(set(pixels))
 
-        gts = np.zeros((len(classes), 3, image_w, image_h))
-        for idx, points in enumerate(classes.values()):
-            gt = gts[idx]
-            for (x, y) in points:
-                for c in range(3):
-                    gt[c][x][y] = 1
+                # Each feature color must have more then 2% pixels
+                current_colors = list(filter(lambda color: color in target_colors or pixels.count(color) > len(pixels) * 0.02, current_colors))
+                if len(current_colors) > len(target_colors):
+                    target_colors = current_colors
+                    target_idx = idx
 
-        return gts
+            self.all_colors = target_colors
+            print(self.all_colors)
+            import matplotlib.pyplot as plt
+            plt.imshow(self.dataset[target_idx][1].transpose((1, 0, 2)))
+            plt.show()
+
+        # parse dataset tuple list through all_colors
+        for gt_index, (image, gt_image) in enumerate(self.dataset):
+            image_w, image_h = gt_image.shape[0:2]
+            classes = {}
+            for w in range(image_w):
+                for h in range(image_h):
+                    color = tuple(gt_image[w][h])
+                    if color not in classes:
+                        classes[color] = []
+                    classes[color].append((w, h))
+
+            gts = np.zeros((len(self.all_colors), 3, image_w, image_h))
+            for idx, color in enumerate(self.all_colors):
+                gt = gts[idx]
+                if color in classes:
+                    for (x, y) in classes[color]:
+                        for c in range(3):
+                            gt[c][x][y] = 1
+
+            self.dataset[gt_index] = (image, gts)
--- unet.py
+++ unet.py
@@ -1,68 +1,95 @@
-import numpy as np
+import argparse
 import time
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
 from model import UNet, loss_fn
 from dataset import SSDataset
 
 
-def train():
+def train(args):
     """
     Train UNet from datasets
     """
-    # hyperparameter
-    batch_size = 128
-    epochs = 5
 
     # dataset
-    train_dataset = SSDataset(dataset_path='./dataset/', is_train=True)
-    test_dataset = SSDataset(dataset_path='./dataset/', is_train=False)
-    train_dataloader = DataLoader(dataset=terain_dataset, batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+    train_dataset = SSDataset(dataset_path=args.dataset_path, is_train=True)
+    val_dataset = SSDataset(dataset_path=args.dataset_path, is_train=False)
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_dataloader = DataLoader(dataset=val_dataset, batch_size=args.batch_size, shuffle=False)
 
     # model
-    net = UNet(in_channels=3, out_channels=4)
-    if torch.cuda.is_available:
+    net = UNet(in_channels=3, out_channels=5)
+    if args.use_gpu:
         net = net.cuda()
 
     # setting
-    lr = 1e-3
-    momentum = 0.99
+    lr = args.lr  # 1e-3
+    momentum = args.momentum  # 0.99
     optimizer = optim.Adam(net.parameters(), lr=lr, momentum=momentum)
     loss_fn = loss_fn
 
-    # train
-    for epoch_idx in range(epochs):
-        model.train()
+    # run
+    train_losses = []
+    val_losses = []
+    for epoch_idx in range(args.epochs):
+        # train
+        net.train()
+        train_loss = 0
         for batch_idx, batch_data in enumerate(train_dataloader):
             xs, ys = batch_data
-            if torch.cuda.is_available:
+            if args.use_gpu:
                 xs = xs.cuda()
                 ys = ys.cuda()
             ys_pred = net(xs)
-
             loss = loss_fn(ys_pred, ys)
+            train_loss += loss
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        model.eval()
-        losses = 0
-        for batch_idx, batch_data in enumerate(test_dataloader):
+        # val
+        net.eval()
+        val_loss = 0
+        for batch_idx, batch_data in enumerate(val_dataloader):
             xs, ys = batch_data
-            if torch.cuda.is_available:
+            if args.use_gpu:
                 xs = xs.cuda()
                 ys = ys.cuda()
             ys_pred = net(xs)
-            losses += loss_fn(ys_pred, ys)
+            loss = loss_fn(ys_pred, ys)
+            val_loss += loss
 
-        print('Epoch: {}, Loss: {}', epoch_idx, losses)
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+        print('Epoch: {}, Train total loss: {}, Val total loss: {}', epoch_idx + 1, train_loss, val_loss)
+
+        # save
+        if (epoch_idx + 1) % args.save_epoch == 0:
+            checkpoint_path = os.path.join(args.checkpoint_path, 'checkpoint_{}.pth'.format(epoch_idx + 1))
+            torch.save(net.state_dict(), checkpoint_path)
+            print('Saved Checkpoint at Epoch {} to {}'.format(epoch_idx + 1, checkpoint_path))
+
+    # summary
+    if args.do_save_summary:
+        epoch_range = list(range(1, args.epochs + 1))
+        plt.plot(epoch_range, train_losses, 'r', label='Train loss')
+        plt.plot(epoch_range, val_loss, 'g', label='Val loss')
+        plt.imsave(args.summary_image)
+        print('Summary images have been saved in {}'.format(args.summary_image))
+
+    # save
+    net.eval()
+    torch.save(net.state_dict(), args.model_state_dict)
+    print('Saved state_dict in {}'.format(args.model_state_dict))
 
 
 def test():
@@ -73,8 +100,34 @@ def test():
 
 
 def main():
-    train()
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--do_train', action='store_true', help='Do train')
+    parser.add_argument('--do_test', action='store_true', help='Do test')
+    parser.add_argument('--use_gpu', action='store_true', help='Use GPU to train and test')
+    parser.add_argument('--dataset_path', type=str, default='./dataset/', help='Dataset folder path')
+    parser.add_argument('--batch_size', type=int, default=128, help='Train and validate batch size')
+    parser.add_argument('--epochs', type=int, default=10, help='Train epoch number')
+    parser.add_argument('--lr', type=float, default=1e-3, help='Adam learning rate')
+    parser.add_argument('--momentum', type=float, default=0.99, help='Adam momentum')
+    parser.add_argument('--save_epoch', type=int, default=1, help='Save checkpoint every epoch')
+    parser.add_argument('--checkpoint_path', type=str, default='./model/', help='Model checkpoint save path')
+    parser.add_argument('--model_state_dict', type=str, default='./model/model.pth', help='Model load and sav path')
+    parser.add_argument('--do_save_summary', action='store_true', help='Do save summary image')
+    parser.add_argument('--summary_image', type=str, default='./summary.png', help='Summary image save path')
+    args = parser.parse_args()
+    assert (args.do_train or args.do_test), 'You must do train or test'
+
+    args.cuda = args.use_gpu and torch.cuda.is_available()
+    print('> Parameters: ')
+    for k, v in zip(args.__dict__.keys(), args.__dict__.values()):
+        print('\t{}: {}'.format(k, v))
+    print('\n')
+
+    if args.do_train:
+        train(args)
+
+    if args.do_test:
+        train(args)
 
 
 if __name__ == '__main__':
